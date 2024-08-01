fc17708b54272a733185b62acbef64bb221ca315
Kiartichai0 <kiartichai.su.61@ubu.ac.th>
2024-08-01 23:20:36 +0700

    upload

---
+++ nuxt-basic/pages/attraction/[id].vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!--h1>{{ items }}</h1-->
+        <h1>{{ items.name }}</h1>
+        <img :src=" items.coverimage">
+        <p>{{ items.detail }}</p>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+const routes = useRoute()
+const items = ref({})
+const id = routes.params.id
+
+console.log(routes.params.id)
+fetch('https://www.melivecode.com/api/attractions/'+id)
+    .then(res => res.json())
+    .then((result) =>{
+        items.value = result.attraction
+    })
+</script>
---
+++ nuxt-basic/pages/attraction/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1> Attractions </h1>
+        <li v-for="i in items">
+            {{ i.name }}
+            <br/>
+            <img :src="i.coverimage"/>
+            <br/>
+            <nuxt-link :to="'/attraction/'+i.id">
+                like to know more?
+            </nuxt-link>
+            <br/>
+        </li>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const items = ref([])
+
+fetch('https://www.melivecode.com/api/attractions')
+    .then(res => res.json())
+    .then((result) =>{
+        items.value = result
+        console.log(result)
+    })
+</script>
